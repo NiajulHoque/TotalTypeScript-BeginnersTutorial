9cc04e6fc6f379e52ce8e8cc7ffe2e3a5ea334e0
Niajul Hoque <nh98.programmer@gmail.com>
2023-05-12 20:26:21 +0300

    Completed Task 15.

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -10,11 +10,13 @@ interface Post {
   body: string;
 }
 
+type UserAndPost = User & { posts: Post[] };
+
 /**
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
+export const getDefaultUserAndPosts = (): UserAndPost => {
   return {
     id: "1",
     firstName: "Matt",
