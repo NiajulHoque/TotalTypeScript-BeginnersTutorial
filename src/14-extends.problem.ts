0aff7b6ddefb92af7a7048e1ed83a7150f21dcbf
Niajul Hoque <nh98.programmer@gmail.com>
2023-05-12 20:12:05 +0300

    Completed Task 14.

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -6,25 +6,26 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
+interface Base {
   id: string;
+}
+
+interface User extends Base {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends Base {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends Base {
   comment: string;
 }
 
 type tests = [
   Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
   Expect<Equal<Post, { id: string; title: string; body: string }>>,
-  Expect<Equal<Comment, { id: string; comment: string }>>,
+  Expect<Equal<Comment, { id: string; comment: string }>>
 ];
